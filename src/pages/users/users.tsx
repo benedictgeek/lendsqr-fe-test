28f9de1cd6fb8bc83d510a0c864706779c83eb86
Olushola <olushola251@gmail.com>
2023-01-21 16:28:01 +0100

    [implemented the users page summary items]

--- src/pages/users/users.tsx
+++ src/pages/users/users.tsx
@@ -1,6 +1,13 @@
+import {
+  ActiveUsersSummaryIcon,
+  UsersSummaryIcon,
+  UsersWithLoansSummaryIcon,
+  UsersWithSavingsSummaryIcon,
+} from "./icons";
 import { ContentBody, PageTitle } from "../../components/layout/helpers";
 import Layout from "../../components/layout/layout";
 import { VerticalSpacer } from "../../components/utils";
+import styles from "./users.module.scss";
 
 const Users = () => {
   return (
@@ -8,9 +15,62 @@ const Users = () => {
       <ContentBody>
         <PageTitle>Users</PageTitle>
         <VerticalSpacer size={40} />
-        
+        <div className={styles.summaryCardsContainer}>
+          <SummaryCard type="users" value={2453} icon={<UsersSummaryIcon />} />
+          <SummaryCard
+            type="activeUsers"
+            value={2453}
+            icon={<ActiveUsersSummaryIcon />}
+          />
+          <SummaryCard
+            type="usersWithLoans"
+            value={12453}
+            icon={<UsersWithLoansSummaryIcon />}
+          />
+          <SummaryCard
+            type="usersWithSavings"
+            value={102453}
+            icon={<UsersWithSavingsSummaryIcon />}
+          />
+        </div>
+        <VerticalSpacer size={20} />
       </ContentBody>
     </Layout>
+  );
+};
+
+interface SummaryCardProps {
+  type: "users" | "activeUsers" | "usersWithLoans" | "usersWithSavings";
+  value: number;
+  icon: JSX.Element;
+}
+
+const SummaryCard = ({ type, value, icon }: SummaryCardProps) => {
+  let title = "";
+  switch (type) {
+    case "users":
+      title = "USERS";
+      break;
+    case "activeUsers":
+      title = "ACTIVE USERS";
+      break;
+    case "usersWithLoans":
+      title = "ACTIVE WITH LOANS";
+      break;
+    case "usersWithSavings":
+      title = "ACTIVE WITH SAVINGS";
+      break;
+    default:
+      break;
+  }
+  return (
+    <div className={styles.summaryCard}>
+      <div className={`${styles.icon} ${styles[type]}`}>{icon}</div>
+      <VerticalSpacer size={14} />
+      <div className={styles.title}>{title}</div>
+      <VerticalSpacer size={12} />
+      <div className={styles.value}>{value.toLocaleString()}</div>
+    </div>
   );
 };
 
