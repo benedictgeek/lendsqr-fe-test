84ed127ecbb2ff9a65ecebf218adf6fd945373d9
Olushola <olushola251@gmail.com>
2023-01-23 19:58:40 +0100

    implemented the additional details secion

--- src/pages/userDetails/userDetails.tsx
+++ src/pages/userDetails/userDetails.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import { Button } from "../../components/button/button";
 import { LongArrowLeft } from "../../components/icons";
 import { ContentBody, PageTitle } from "../../components/layout/helpers";
@@ -26,8 +27,61 @@ const UserDetails = () => {
         </div>
         <VerticalSpacer size={40} />
         <UserSummary />
+        <VerticalSpacer size={30} />
+        <div className={styles.additionalInformation}>
+          <AdditionalInformationSection
+            headerText={"Personal Information"}
+            values={[
+              { title: "Full Name", value: "Grame me" },
+              { title: "Full Name", value: "Grame me" },
+            ]}
+          />
+          <AdditionalInformationSection
+            headerText={"Personal Information"}
+            values={[{ title: "Full Name", value: "Grame me" }]}
+          />
+          <AdditionalInformationSection
+            headerText={"Personal Information"}
+            values={[{ title: "Full Name", value: "Grame me" }]}
+          />
+          <AdditionalInformationSection
+            headerText={"Personal Information"}
+            values={[{ title: "Full Name", value: "Grame me" }]}
+          />
+        </div>
       </ContentBody>
     </Layout>
+  );
+};
+
+type InfoItemType = {
+  title: ReactNode;
+  value: ReactNode;
+};
+
+interface AdditionalInformationSectionProps {
+  headerText?: ReactNode;
+  values: InfoItemType[];
+}
+
+const AdditionalInformationSection = ({
+  headerText,
+  values,
+}: AdditionalInformationSectionProps) => {
+  return (
+    <div className={styles.section}>
+      <div className={styles.header}>{headerText}</div>
+      <div className={styles.itemsContainer}>
+        {values.map((item, index) => {
+          return (
+            <div key={`${headerText}_item_${index}`} className={styles.item}>
+              <div className={styles.title}>{item.title}</div>
+              <div className={styles.value}>{item.value}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
